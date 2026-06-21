740acf475d533a32fc00e7685b799e17e14e7e9e
your_username <saadchadu@gmail.com>
2026-06-21 20:07:36 +0530

    Add 5 new results: HS urdu-kavitha+prasangam-english, Senior prasangam-mal+eng, General moulid

--- results-manifest.js
+++ results-manifest.js
@@ -4,9 +4,9 @@
 window.RESULTS_MANIFEST = {
   "LP": ["katha-kathanam", "quiz", "pencil-drawing", "madh-ganam", "jalachayam", "hand-writing-girls", "vayana-mal", "vayana-arabi-mal"],
   "UP": ["pencil-drawing", "quiz", "spelling-bee", "ghanithakeli", "jalachayam", "katharachana", "katha-kathanam", "mappilappattu", "katharachana-girls", "jalachayam-girls", "prasangam-malayalam", "book-test"],
-  "HS": ["caption-writing", "katha-rachana", "kavitha-rachana", "language-game", "essay-malayalam", "news-reading", "quiz", "pencil-drawing", "water-colour", "kavitha-parayanam-malayalam", "mappilappattu", "pencil-drawing-girls", "water-colour-girls", "katha-rachana-girls"],
+  "HS": ["caption-writing", "katha-rachana", "kavitha-rachana", "language-game", "essay-malayalam", "news-reading", "quiz", "pencil-drawing", "water-colour", "kavitha-parayanam-malayalam", "mappilappattu", "pencil-drawing-girls", "water-colour-girls", "katha-rachana-girls", "urdu-kavitha-parayanam", "prasangam-english"],
   "HSS": ["essay-english", "katha-rachana", "pencil-drawing", "arabic-calligraphy", "water-colour", "urdu-kavitha-parayanam", "digital-painting", "bhakthiganam", "arabic-calligraphy-girls", "embroidery-girls", "mappilappattu", "kavitha-rachana-girls", "prasangam-malayalam"],
   "Junior": ["katha-rachana", "kavitha-rachana", "calligraphy", "essay-arabi", "social-text", "essay-malayalam", "translation-arabi", "madhgana-rachana", "mappilappattu", "prasangam-english"],
-  "Senior": ["feature-writing", "katha-rachana", "kavitha-rachana", "e-poster", "translation-english", "social-text", "kavitha-rachana-english", "mudravakya-rachana", "essay-english", "essay-malayalam", "mappilappattu"],
-  "General": ["nasheeda", "collage"]
+  "Senior": ["feature-writing", "katha-rachana", "kavitha-rachana", "e-poster", "translation-english", "social-text", "kavitha-rachana-english", "mudravakya-rachana", "essay-english", "essay-malayalam", "mappilappattu", "prasangam-malayalam", "prasangam-english"],
+  "General": ["nasheeda", "collage", "moulid-parayanam"]
 };
