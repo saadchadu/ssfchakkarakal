a25ca6aed2f0c566047da6e6a0e6f39ce5346daf
your_username <saadchadu@gmail.com>
2026-06-21 22:46:14 +0530

    Add 13 new results: LP bhasha-keli+pencil-drawing-girls+book-test+jalachayam-girls, UP origami-girls, HS prasangam-malayalam+news-reading, HSS book-test, Senior book-test, General spot-magazine+chumarezhuth+mappila-song-writing+group-song-cat-a

--- results-manifest.js
+++ results-manifest.js
@@ -2,11 +2,11 @@
 // Now uses folder structure: results/Category/program-name/poster.jpg (or .png/.jpeg/.webp)
 // Just drop poster.jpg into the correct folder and update this list.
 window.RESULTS_MANIFEST = {
-  "LP": ["katha-kathanam", "quiz", "pencil-drawing", "madh-ganam", "jalachayam", "hand-writing-girls", "vayana-mal", "vayana-arabi-mal", "prasangam-malayalam"],
-  "UP": ["pencil-drawing", "quiz", "spelling-bee", "ghanithakeli", "jalachayam", "katharachana", "katha-kathanam", "mappilappattu", "katharachana-girls", "jalachayam-girls", "prasangam-malayalam", "book-test"],
-  "HS": ["caption-writing", "katha-rachana", "kavitha-rachana", "language-game", "essay-malayalam", "news-reading", "quiz", "pencil-drawing", "water-colour", "kavitha-parayanam-malayalam", "mappilappattu", "pencil-drawing-girls", "water-colour-girls", "katha-rachana-girls", "urdu-kavitha-parayanam", "prasangam-english", "book-test"],
-  "HSS": ["essay-english", "katha-rachana", "pencil-drawing", "arabic-calligraphy", "water-colour", "urdu-kavitha-parayanam", "digital-painting", "bhakthiganam", "arabic-calligraphy-girls", "embroidery-girls", "mappilappattu", "kavitha-rachana-girls", "prasangam-malayalam"],
+  "LP": ["katha-kathanam", "quiz", "pencil-drawing", "madh-ganam", "jalachayam", "hand-writing-girls", "vayana-mal", "vayana-arabi-mal", "prasangam-malayalam", "bhasha-keli", "pencil-drawing-girls", "book-test", "jalachayam-girls"],
+  "UP": ["pencil-drawing", "quiz", "spelling-bee", "ghanithakeli", "jalachayam", "katharachana", "katha-kathanam", "mappilappattu", "katharachana-girls", "jalachayam-girls", "prasangam-malayalam", "book-test", "origami-girls"],
+  "HS": ["caption-writing", "katha-rachana", "kavitha-rachana", "language-game", "essay-malayalam", "news-reading", "quiz", "pencil-drawing", "water-colour", "kavitha-parayanam-malayalam", "mappilappattu", "pencil-drawing-girls", "water-colour-girls", "katha-rachana-girls", "urdu-kavitha-parayanam", "prasangam-english", "book-test", "prasangam-malayalam"],
+  "HSS": ["essay-english", "katha-rachana", "pencil-drawing", "arabic-calligraphy", "water-colour", "urdu-kavitha-parayanam", "digital-painting", "bhakthiganam", "arabic-calligraphy-girls", "embroidery-girls", "mappilappattu", "kavitha-rachana-girls", "prasangam-malayalam", "book-test"],
   "Junior": ["katha-rachana", "kavitha-rachana", "calligraphy", "essay-arabi", "social-text", "essay-malayalam", "translation-arabi", "madhgana-rachana", "mappilappattu", "prasangam-english", "socio-synapse", "quiz", "prasangam-malayalam"],
-  "Senior": ["feature-writing", "katha-rachana", "kavitha-rachana", "e-poster", "translation-english", "social-text", "kavitha-rachana-english", "mudravakya-rachana", "essay-english", "essay-malayalam", "mappilappattu", "prasangam-malayalam", "prasangam-english", "hamd-urdu", "kavitha-parayanam-english"],
-  "General": ["nasheeda", "collage", "moulid-parayanam", "viplava-gana-rachana"]
+  "Senior": ["feature-writing", "katha-rachana", "kavitha-rachana", "e-poster", "translation-english", "social-text", "kavitha-rachana-english", "mudravakya-rachana", "essay-english", "essay-malayalam", "mappilappattu", "prasangam-malayalam", "prasangam-english", "hamd-urdu", "kavitha-parayanam-english", "book-test"],
+  "General": ["nasheeda", "collage", "moulid-parayanam", "viplava-gana-rachana", "spot-magazine", "chumarezhuth", "mappila-song-writing", "group-song-cat-a"]
 };
