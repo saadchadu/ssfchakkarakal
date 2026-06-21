c9c1bab8718527d6849e5bbd4c4da0ef269ba1cf
your_username <saadchadu@gmail.com>
2026-06-21 11:18:36 +0530

    Use local manifest for results - no more GitHub API dependency

---
+++ results-manifest.js
@@ -0,0 +1,11 @@
+// Auto-generated manifest of published results.
+// Update this file whenever you add/remove result posters.
+window.RESULTS_MANIFEST = {
+  "LP": [],
+  "UP": [],
+  "HS": ["Caption Writing.png", "katha rachana.png", "kavitha rachana.png"],
+  "HSS": ["Essay English.png", "katha rachana.png"],
+  "Junior": ["Katha Rachana.png", "Kavitha Rachana.png"],
+  "Senior": ["Katha Rachana.png", "Kavitha Rachana.png", "feature writing.png"],
+  "General": []
+};
