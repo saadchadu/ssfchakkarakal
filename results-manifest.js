412ed0c50cb9d56079fb0830ee26ac08db9178b4
your_username <saadchadu@gmail.com>
2026-06-21 17:31:55 +0530

    Add 5 new results, rename to poster.jpg

--- results-manifest.js
+++ results-manifest.js
@@ -3,10 +3,10 @@
 // Just drop poster.jpg into the correct folder and update this list.
 window.RESULTS_MANIFEST = {
   "LP": ["katha-kathanam", "quiz", "pencil-drawing"],
-  "UP": ["pencil-drawing", "quiz", "spelling-bee", "ghanithakeli", "jalachayam"],
+  "UP": ["pencil-drawing", "quiz", "spelling-bee", "ghanithakeli", "jalachayam", "katharachana"],
   "HS": ["caption-writing", "katha-rachana", "kavitha-rachana", "language-game", "essay-malayalam", "news-reading", "quiz", "pencil-drawing", "water-colour"],
-  "HSS": ["essay-english", "katha-rachana", "pencil-drawing"],
+  "HSS": ["essay-english", "katha-rachana", "pencil-drawing", "arabic-calligraphy", "water-colour"],
   "Junior": ["katha-rachana", "kavitha-rachana", "calligraphy", "essay-arabi", "social-text", "essay-malayalam", "translation-arabi", "madhgana-rachana"],
-  "Senior": ["feature-writing", "katha-rachana", "kavitha-rachana", "e-poster", "translation-english", "social-text"],
+  "Senior": ["feature-writing", "katha-rachana", "kavitha-rachana", "e-poster", "translation-english", "social-text", "kavitha-rachana-english", "mudravakya-rachana"],
   "General": []
 };
