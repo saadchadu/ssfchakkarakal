2feed75f3f21bb71c76a5c922641f38679933261
your_username <saadchadu@gmail.com>
2026-06-21 15:55:41 +0530

    Add new result posters, rename to poster.jpg, update manifest

--- results-manifest.js
+++ results-manifest.js
@@ -2,11 +2,11 @@
 // Now uses folder structure: results/Category/program-name/poster.jpg (or .png/.jpeg/.webp)
 // Just drop poster.jpg into the correct folder and update this list.
 window.RESULTS_MANIFEST = {
-  "LP": ["katha-kathanam"],
-  "UP": ["pencil-drawing"],
-  "HS": ["caption-writing", "katha-rachana", "kavitha-rachana", "language-game", "essay-malayalam", "news-reading", "quiz"],
+  "LP": ["katha-kathanam", "quiz"],
+  "UP": ["pencil-drawing", "quiz", "spelling-bee", "ghanithakeli"],
+  "HS": ["caption-writing", "katha-rachana", "kavitha-rachana", "language-game", "essay-malayalam", "news-reading", "quiz", "pencil-drawing", "water-colour"],
   "HSS": ["essay-english", "katha-rachana", "pencil-drawing"],
-  "Junior": ["katha-rachana", "kavitha-rachana", "calligraphy", "essay-arabi"],
-  "Senior": ["feature-writing", "katha-rachana", "kavitha-rachana"],
+  "Junior": ["katha-rachana", "kavitha-rachana", "calligraphy", "essay-arabi", "social-text", "essay-malayalam"],
+  "Senior": ["feature-writing", "katha-rachana", "kavitha-rachana", "e-poster", "translation-english"],
   "General": []
 };
