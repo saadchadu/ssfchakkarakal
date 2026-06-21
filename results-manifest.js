59c855448debafd158a9454df8af20ff198f708e
your_username <saadchadu@gmail.com>
2026-06-21 19:05:54 +0530

    Add 10 new results (including girls categories), update manifest and program lists

--- results-manifest.js
+++ results-manifest.js
@@ -2,10 +2,10 @@
 // Now uses folder structure: results/Category/program-name/poster.jpg (or .png/.jpeg/.webp)
 // Just drop poster.jpg into the correct folder and update this list.
 window.RESULTS_MANIFEST = {
-  "LP": ["katha-kathanam", "quiz", "pencil-drawing", "madh-ganam"],
-  "UP": ["pencil-drawing", "quiz", "spelling-bee", "ghanithakeli", "jalachayam", "katharachana", "katha-kathanam", "mappilappattu"],
-  "HS": ["caption-writing", "katha-rachana", "kavitha-rachana", "language-game", "essay-malayalam", "news-reading", "quiz", "pencil-drawing", "water-colour", "kavitha-parayanam-malayalam"],
-  "HSS": ["essay-english", "katha-rachana", "pencil-drawing", "arabic-calligraphy", "water-colour", "urdu-kavitha-parayanam", "digital-painting", "bhakthiganam"],
+  "LP": ["katha-kathanam", "quiz", "pencil-drawing", "madh-ganam", "jalachayam", "hand-writing-girls"],
+  "UP": ["pencil-drawing", "quiz", "spelling-bee", "ghanithakeli", "jalachayam", "katharachana", "katha-kathanam", "mappilappattu", "katharachana-girls", "jalachayam-girls"],
+  "HS": ["caption-writing", "katha-rachana", "kavitha-rachana", "language-game", "essay-malayalam", "news-reading", "quiz", "pencil-drawing", "water-colour", "kavitha-parayanam-malayalam", "mappilappattu", "pencil-drawing-girls", "water-colour-girls", "katha-rachana-girls"],
+  "HSS": ["essay-english", "katha-rachana", "pencil-drawing", "arabic-calligraphy", "water-colour", "urdu-kavitha-parayanam", "digital-painting", "bhakthiganam", "arabic-calligraphy-girls", "embroidery-girls"],
   "Junior": ["katha-rachana", "kavitha-rachana", "calligraphy", "essay-arabi", "social-text", "essay-malayalam", "translation-arabi", "madhgana-rachana"],
   "Senior": ["feature-writing", "katha-rachana", "kavitha-rachana", "e-poster", "translation-english", "social-text", "kavitha-rachana-english", "mudravakya-rachana", "essay-english", "essay-malayalam"],
   "General": []
