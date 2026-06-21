1013f9f4a367a29295aac0ee304e258fd4ccd709
your_username <saadchadu@gmail.com>
2026-06-21 17:56:44 +0530

    Add 10 new results, rename to poster.jpg

--- results-manifest.js
+++ results-manifest.js
@@ -2,11 +2,11 @@
 // Now uses folder structure: results/Category/program-name/poster.jpg (or .png/.jpeg/.webp)
 // Just drop poster.jpg into the correct folder and update this list.
 window.RESULTS_MANIFEST = {
-  "LP": ["katha-kathanam", "quiz", "pencil-drawing"],
-  "UP": ["pencil-drawing", "quiz", "spelling-bee", "ghanithakeli", "jalachayam", "katharachana"],
-  "HS": ["caption-writing", "katha-rachana", "kavitha-rachana", "language-game", "essay-malayalam", "news-reading", "quiz", "pencil-drawing", "water-colour"],
-  "HSS": ["essay-english", "katha-rachana", "pencil-drawing", "arabic-calligraphy", "water-colour"],
+  "LP": ["katha-kathanam", "quiz", "pencil-drawing", "madh-ganam"],
+  "UP": ["pencil-drawing", "quiz", "spelling-bee", "ghanithakeli", "jalachayam", "katharachana", "katha-kathanam", "mappilappattu"],
+  "HS": ["caption-writing", "katha-rachana", "kavitha-rachana", "language-game", "essay-malayalam", "news-reading", "quiz", "pencil-drawing", "water-colour", "kavitha-parayanam-malayalam"],
+  "HSS": ["essay-english", "katha-rachana", "pencil-drawing", "arabic-calligraphy", "water-colour", "urdu-kavitha-parayanam", "digital-painting", "bhakthiganam"],
   "Junior": ["katha-rachana", "kavitha-rachana", "calligraphy", "essay-arabi", "social-text", "essay-malayalam", "translation-arabi", "madhgana-rachana"],
-  "Senior": ["feature-writing", "katha-rachana", "kavitha-rachana", "e-poster", "translation-english", "social-text", "kavitha-rachana-english", "mudravakya-rachana"],
+  "Senior": ["feature-writing", "katha-rachana", "kavitha-rachana", "e-poster", "translation-english", "social-text", "kavitha-rachana-english", "mudravakya-rachana", "essay-english", "essay-malayalam"],
   "General": []
 };
