e887a473b6b71e89b0a47935bd412270c0efe991
your_username <saadchadu@gmail.com>
2026-06-21 15:39:13 +0530

    Restructure results: folder per program, poster.jpg naming convention

--- results-manifest.js
+++ results-manifest.js
@@ -1,11 +1,12 @@
 // Auto-generated manifest of published results.
-// Update this file whenever you add/remove result posters.
+// Now uses folder structure: results/Category/program-name/poster.jpg (or .png/.jpeg/.webp)
+// Just drop poster.jpg into the correct folder and update this list.
 window.RESULTS_MANIFEST = {
-  "LP": [],
-  "UP": [],
-  "HS": ["caption-writing.png", "katha-rachana.png", "kavitha-rachana.png"],
-  "HSS": ["essay-english.png", "katha-achana.png"],
-  "Junior": ["katha-achana.png", "kavitha-rachana.png"],
-  "Senior": ["feature-writing.png", "katha-rachana.png", "kavitha-achana.png"],
+  "LP": ["katha-kathanam"],
+  "UP": ["pencil-drawing"],
+  "HS": ["caption-writing", "katha-rachana", "kavitha-rachana", "language-game", "essay-malayalam", "news-reading", "quiz"],
+  "HSS": ["essay-english", "katha-rachana", "pencil-drawing"],
+  "Junior": ["katha-rachana", "kavitha-rachana", "calligraphy", "essay-arabi"],
+  "Senior": ["feature-writing", "katha-rachana", "kavitha-rachana"],
   "General": []
 };
